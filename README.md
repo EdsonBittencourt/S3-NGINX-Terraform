25a3faba555d519585cfe46fd517b54ed9c723de
Edson Bittencourt <edson@Edsons-MacBook-Air.local>
2022-01-17 23:35:07 -0300

    adding first files

--- README.md
+++
@@ -1,2 +0,0 @@
-# S3-Nginx-Terraform
-Terraform code with remote state stored in S3 bucket.
---
+++ backend.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+    }
+  }
+  required_version = ">= 0.13"
+  backend "s3" {
+    profile = "demo"
+    region  = ""
+    key     = "terraform.tfstate"
+    bucket  = ""
+  }
+}
